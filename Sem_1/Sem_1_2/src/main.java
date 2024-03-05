412552dc519336e0d7193527948166ff8ca23365
Pavel <pavel@b2profit.ru>
2024-03-05 16:01:19 +0300

    Весь 1 семинар

--- Sem_1/Sem_1_2/src/main.java
+++ Sem_1/Sem_1_2/src/main.java
@@ -18,27 +18,32 @@ public class main {
      * (пример: ПродающийБутылкиВодыАвтомат)*/
 
     public static void main(String[] args){
-        Product product_1 = new Product();
+//        Product product_1 = new Product();
+//        product_1.setName("Кофе 200 мл");
+//        product_1.setCost(150.0);
+
+        Cofe product_1 = new Cofe();
         product_1.setName("Кофе 200 мл");
         product_1.setCost(150.0);
 
-        Product product_2 = new Product();
+
+        Cofe product_2 = new Cofe();
         product_2.setName("Кофе 300 мл");
         product_2.setCost(200.0);
 
-        Product product_3 = new Product();
-        product_1.setName("Кофе 400 мл");
-        product_1.setCost(250.0);
+        Cofe product_3 = new Cofe();
+        product_3.setName("Кофе 400 мл");
+        product_3.setCost(250.0);
 
-        Product product_4 = new Product();
+        Beer product_4 = new Beer();
         product_4.setName("Пиво Guinness 330 мл");
         product_4.setCost(200.0);
 
-        Product product_5 = new Product();
+        Beer product_5 = new Beer();
         product_5.setName("Пиво Guinness 500 мл");
         product_5.setCost(300.0);
 
-        Product product_6 = new Product();
+        Beer product_6 = new Beer();
         product_6.setName("Пиво Guinness 500 мл");
         product_6.setCost(60.0);
 
