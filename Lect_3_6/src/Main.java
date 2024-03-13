793620bafcb144d336620d73bd6960305ab70338
Pavel <pavel@b2profit.ru>
2024-03-13 17:27:47 +0300

    Часть 5 Equals 1способ 3 лекция

---
+++ Lect_3_6/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Coffee latte = new Coffee();
+        latte.addComponent(new Water("вода"));
+        latte.addComponent(new Water("вода"));
+        latte.addComponent(new Water("вода"));
+        for (var ingredient : latte) {
+            System.err.println(ingredient);
+    }
+}
