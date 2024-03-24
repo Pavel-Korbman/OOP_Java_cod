19382ffc5ff3249632e2fc43eb7cccce155114e8
Pavel <pavel@b2profit.ru>
2024-03-24 18:17:12 +0300

    2 часть 6 семинара

---
+++ Sem_6_2/src/Employee.java
@@ -0,0 +1,14 @@
+public class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+}
+
+class SaveEmployee{
+    public void saveEmployee(Employee employee){
+        //Сохранение данных сотрудника в базу
+    }
+}
---
+++ Sem_6_2/src/Salary.java
@@ -0,0 +1,5 @@
+public class Salary {
+    public void calculateSalary(Employee employee) {
+        //Рассчёт зарплаты
+    }
+}
