1726b8727655ad0b0fe92752fb3186aac984639e
Pavel <pavel@b2profit.ru>
2024-03-24 18:59:38 +0300

    3 часть 6 семинара

---
+++ Sem_6_3/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(2.0, 6.0);
+        Circle circle = new Circle(3.0);
+
+        System.out.println(rectangle.calculateArea());
+        System.out.println(circle.calculateArea());
+
+    }
+}
