29a98ba56c0bb212ec96f6a881ced1a64a57e89c
Pavel <pavel@b2profit.ru>
2024-03-24 17:41:45 +0300

    1 часть 6 семинара

--- Sem_6_1/src/Main.java
+++ Sem_6_1/src/Main.java
@@ -1,6 +1,100 @@
 
 public class Main {
     public static void main(String[] args) {
+        AnimalDatabase database = new AnimalDatabase();
 
+        Animal dog = new Dog("Rex", 20.5);
+        database.saveAnimal(dog);
+
+        Animal penguin = new Penguin("Tux", 10.2);
+        database.saveAnimal(penguin);
+
+        Animal bear = new Bear("Baloo", 200);
+        database.saveAnimal(bear);
+    }
+}
+
+/**
+ * Примеры классов, демонстрирующие SOLID
+ */
+
+/**
+ * Принцип единственной ответственности (SRP)
+ */
+class Animal {
+    private String name;
+    private double weight;
+
+    public Animal(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    // Геттеры и сеттеры для name и weight
+
+}
+
+class AnimalDatabase {
+    public void saveAnimal(Animal animal) {
+        // Сохранение данных животного в базу - в отдельном классе
+    }
+}
+
+/**
+ * Принцип открытости/ закрытости (OCP)
+ */
+
+interface Movable {
+    void move();
+}
+
+class Dog extends Animal implements Movable {
+    public Dog(String name, double weight) {
+        super(name, weight);
+    }
+
+    @Override
+    public void move() {
+        // Реализация движения собаки
+    }
+}
+
+class Bird extends Animal implements Movable {
+    public Bird(String name, double weight) {
+        super(name, weight);
+    }
+
+    @Override
+    public void move() {
+        // Реализация полёта птицы
+    }
+}
+/**
+ * Принцип подстановки Барбары Лисков (LSP)
+ */
+class Penguin extends Bird {
+    public Penguin(String name, double weight) {
+        super(name, weight);
+    }
+    //Пингвины не летают, по этому метод move() - не переопределяется (интерфейс Movable не имплементируем)
+}
+
+/**
+ * Принцип разделения интерфейса (ISP)
+ */
+interface Eatable {
+    void eat();
+}
+class Bear extends Animal implements Eatable, Movable {
+    public Bear(String name, double weight) {
+        super(name, weight);
+    }
+    @Override
+    public void move(){
+        //Реализация движения медведя
+    }
+    @Override
+    public void eat(){
+        //Реализация питания медведя
     }
 }
