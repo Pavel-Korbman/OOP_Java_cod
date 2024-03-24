3156fa9a3d1806399b5b8ba49bf2e987e32d3ea1
Pavel <pavel@b2profit.ru>
2024-03-24 19:18:49 +0300

    4 часть 6 семинара

---
+++ Sem_6_4/src/Bird.java
@@ -0,0 +1,18 @@
+public class Bird {
+    public void fly() {
+        // Реализация полёта птицы
+    }
+}
+
+//class Ostrich extends Bird {
+//    public void fly() {
+//        throw new UnsupportedOperationException("Статус - не умеет летать");
+//    }
+//}
+
+class Ostrich extends Bird {
+//    Статус - не умеет летать - метод не реализовываем
+}
+
+
+
