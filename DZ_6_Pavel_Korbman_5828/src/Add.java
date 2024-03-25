3be1d430ba6493abbbc359c8eb5367070a1ace59
Pavel <pavel@b2profit.ru>
2024-03-25 04:14:33 +0300

    6 ДЗ Корбман 5828

--- DZ_6_Pavel_Korbman_5828/src/Add.java
+++ DZ_6_Pavel_Korbman_5828/src/Add.java
@@ -1,3 +1,8 @@
+/**
+ * Метод можно имплементировать в розничных складах и не имплементировать в оптовых,
+ * где добавление только списком Interface segregation principle
+ * @param <Object>
+ */
 public interface Add<Object>{
     public abstract void add(Object product, Object supplier);
 
